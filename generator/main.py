529c9512f291523c617c381e93b77866ed6f45ba
d-zaytsev <dzaytsev308@gmail.com>
2024-11-13 21:21:46 +0300

    Optimize cats

--- generator/main.py
+++ generator/main.py
@@ -51,13 +51,10 @@ class CatGenerator:
         self.__angle_array_sin[cat_ids] = rads_sin
 
     def move_cats(self):
-        for cat_id in range(self.__CATS_COUNT):
-            x, y = self.__cat_matrix[0][cat_id], self.__cat_matrix[1][cat_id]
+        # Adjust the rotated points by the radius
+        self.__cat_matrix[0] += self.__RADIUS * self.__angle_array_cos
+        self.__cat_matrix[1] += self.__RADIUS * self.__angle_array_sin
 
-            new_x = x + self.__RADIUS * self.__angle_array_cos[cat_id]
-            new_y = y + self.__RADIUS * self.__angle_array_sin[cat_id]
-
-            self.__cat_matrix[0][cat_id], self.__cat_matrix[1][cat_id] = new_x, new_y
 
     def restrict_cat(self, cat_id: int):
         """Return back the cat that escaped abroad into the playing field."""
@@ -77,21 +74,14 @@ class CatGenerator:
         
         self.__cat_matrix[0][cat_id], self.__cat_matrix[1][cat_id] = new_x, new_y
 
-# gen = CatGenerator(N=5*(10**5), R=300,x_border=10000, y_border=10000)
-# xs, ys = gen.cats
-# gen.update_cats()
 
+import time
 x_border, y_border = 1000, 1000
-
-gen = CatGenerator(N=5*(10), R=100,x_border=x_border, y_border=y_border)
+gen = CatGenerator(N=5*(10**5), R=100,x_border=x_border, y_border=y_border)
 
 for i in range(10):
     xs, ys = gen.cats
 
-    plt.axis([0, x_border, 0, y_border])
-    plt.scatter(xs, ys, c='blue')
-    plt.title('Cats !!!')
-
-    plt.savefig(f'{i}.png')
-    plt.close()
+    start = time.perf_counter()
     gen.update_cats()
+    print(time.perf_counter() - start)
