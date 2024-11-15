803a4d82c6c37b598a2dcf9bf4b939ca5b629684
d-zaytsev <dzaytsev308@gmail.com>
2024-11-15 18:58:47 +0300

    Implement bad borders

--- generator/main.py
+++ generator/main.py
@@ -1,6 +1,8 @@
+from typing import Tuple
 import numpy as np
 import random
 import time
+import matplotlib.pyplot as plt
 
 
 class CatGenerator:
@@ -22,6 +24,8 @@ class CatGenerator:
         self.__cos_array = np.cos(angle_array)
         self.__sin_array = np.sin(angle_array)
 
+        self.__bad_borders = []
+
     @property
     def cats(self):
         return self.__cat_matrix[0], self.__cat_matrix[1]
@@ -31,6 +35,9 @@ class CatGenerator:
         self.__update_angles()
         self.__move_cats()
         self.__restrict_cats()
+
+    def add_bad_border(self, a: Tuple[int, int], b: Tuple[int, int]):
+        self.__bad_borders.append([*a, *b])
 
     def __update_angles(self):
         """Randomly updates the angles of a cats subset.
@@ -49,8 +56,37 @@ class CatGenerator:
 
     def __move_cats(self):
         """Move all cats based on their current angles."""
-        self.__cat_matrix[0] += self.__RADIUS * self.__cos_array
-        self.__cat_matrix[1] += self.__RADIUS * self.__sin_array
+        xs, ys = self.__cat_matrix[0], self.__cat_matrix[1]
+
+        # find points where cats want to move
+        new_xs = xs + self.__RADIUS * self.__cos_array
+        new_ys = ys + self.__RADIUS * self.__sin_array
+
+        # look through all the bad borders
+        for x0, y0, x1, y1 in self.__bad_borders:
+            # find all intersections of cats with bad border
+            ids, intersection_xs, intersection_ys = self.__find_intersections(
+                xs, ys, new_xs, new_ys, x0, y0, x1, y1
+            )
+            # ids -> id of cats that tried to intersect bad border
+
+            if len(ids) == 0:
+                continue
+
+            # return back these cats (but with small offset otherwise they will get stuck at the border)
+            new_xs[ids], new_ys[ids] = self.__offset_points(xs[ids], ys[ids], intersection_xs, intersection_ys)
+
+        self.__cat_matrix[0], self.__cat_matrix[1] = new_xs, new_ys
+
+    def __offset_points(self, xs, ys, intersection_xs, intersection_ys, offset_factor=0.1):
+        offset_x = (intersection_xs - xs) * offset_factor
+        offset_y = (intersection_ys - ys) * offset_factor
+
+        new_xs = xs + offset_x
+        new_ys = ys + offset_y
+
+        return (new_xs, new_ys)
+
 
     def __restrict_cats(self):
         """Keeps all cats within the boundaries."""
@@ -64,22 +100,55 @@ class CatGenerator:
 
         self.__cat_matrix[0], self.__cat_matrix[1] = x, y
 
+    def __find_intersections(self, xs, ys, new_xs, new_ys, x0, y0, x1, y1):
+        trajectories = (new_xs - xs, new_ys - ys)
+        bad_zone = (x1 - x0, y1 - y0)
+
+        denominators = (trajectories[0] * bad_zone[1]) - (trajectories[1] * bad_zone[0])
+
+        t = np.divide(
+            (x0 - xs) * bad_zone[1] - (y0 - ys) * bad_zone[0],
+            denominators,
+            out=np.zeros_like(denominators),
+            where=denominators != 0,
+        )
+        u = np.divide(
+            (x0 - xs) * trajectories[1] - (y0 - ys) * trajectories[0],
+            denominators,
+            out=np.zeros_like(denominators),
+            where=denominators != 0,
+        )
+
+        mask = (t >= 0) & (t <= 1) & (u >= 0) & (u <= 1)
+        ids = np.where(mask)[0]
+
+        x = xs[ids] + t[ids] * trajectories[0][ids]
+        y = ys[ids] + t[ids] * trajectories[1][ids]
+
+        return (ids, x, y)
+
 
 x_border, y_border = 1000, 1000
-gen = CatGenerator(N=5 * (10**5), R=100, x_border=x_border, y_border=y_border)
+gen = CatGenerator(N=10, R=100, x_border=x_border, y_border=y_border)
+
+gen.add_bad_border((100, 100), (100, 800))
+gen.add_bad_border((800, 100), (800, 800))
+gen.add_bad_border((800, 800), (100, 800))
+gen.add_bad_border((100, 800), (100, 100))
+
 
 for i in range(10):
     xs, ys = gen.cats
 
-    start = time.perf_counter()
-    gen.update_cats()
+    # plt.scatter(xs, ys, c="blue")
+    # plt.title("Cats !!!")
 
-    # plt.scatter(xs, ys, c='blue')
-    # plt.title('Cats !!!')
-
+    # plt.plot([100, 100, 800, 800, 100], [100, 800, 800, 100, 100], "r-")
     # plt.axis([0, x_border, 0, y_border])
 
-    # plt.savefig(f'{i}.png')
+    # plt.savefig(f"{i}.png")
     # plt.close()
 
+    start = time.perf_counter()
+    gen.update_cats()
     print(f"{i}:{time.perf_counter() - start}")
